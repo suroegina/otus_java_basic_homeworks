0d42b9998bbd7cc795b8449b8040255eb914f5cf
Daria Suroegina <SuroeginaDM@multicarta.ru>
2025-01-30 17:21:04 +0300

    homework #24 update

--- src/main/java/ru/otus/https/server/HttpServer.java
+++ src/main/java/ru/otus/https/server/HttpServer.java
@@ -3,8 +3,6 @@ package ru.otus.https.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
